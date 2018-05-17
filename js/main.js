19bad994f8e70b7fe1dddfb80fc05dd9163297c3
geekbrucy <peichunzhou1006@gmail.com>
2018-05-17 20:47:45 +1000

    layout style finished

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// // getInfo()
+// var timer = null
+// if(timer) {
+//   clearTimeout(timer)
+// }
+// timer = setTimeout(getInfo,50)
+
+
+// function getInfo () {
+// 	fetch('https://fcc-weather-api.glitch.me/api/current?lat=-35&lon=‎149')
+// 	.then(function (res) {
+// 		return res.json();
+// 	})
+// 	.then(function (data) {
+// 		console.log(data)
+// 	})
+// }
